995346bb72e6e6ebc5b44e1caf000b65c8d62c25
Erin Cochran <erin.k.cochran@gmail.com>
2020-04-07 14:34:01 -0400

    Add javascript for copy button

---
+++ js/copyCode.js
@@ -0,0 +1,29 @@
+const copyCode = copyCodeButton => {
+  const tempTextArea = document.createElement("textarea");
+  tempTextArea.textContent = copyCodeButton.getAttribute("data-code");
+  document.body.appendChild(tempTextArea);
+
+  const selection = document.getSelection();
+  selection.removeAllRanges();
+  tempTextArea.select();
+  document.execCommand("copy");
+  selection.removeAllRanges();
+  document.body.removeChild(tempTextArea);
+
+  copyCodeButton.classList.add("copied");
+setTimeout(() => {
+  copyCodeButton.classList.remove("copied");
+}, 2000);
+};
+
+document.addEventListener("keyup", keyEvent => {
+  if (keyEvent.keyCode === 13) {
+    copyCode(keyEvent.target);
+  }
+});
+
+document.querySelectorAll(".copy-code-button").forEach(copyCodeButton => {
+  copyCodeButton.addEventListener("click", clickEvent =>
+    copyCode(clickEvent.target)
+  );
+});
