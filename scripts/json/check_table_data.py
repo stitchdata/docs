38857ddcd1418d59217f83727c3c9c4572a8f499
Lucie Milan <lmilan@talend.com>
2023-10-04 10:18:08 +0200

    check missing json files

--- scripts/json/check_table_data.py
+++ scripts/json/check_table_data.py
@@ -56,30 +56,35 @@ def checkPrimaryReplicationKeys(file, json_folder, issue_list):
                     replication_keys = details['replication-keys']
                 except:
                     replication_keys = []
-            
-            with open(json_file, 'r', encoding='utf-8') as j:
-                json_content = json.load(j)
-                properties = json_content['properties']
 
-                if len(primary_keys) > 0:
-                    for primary_key in primary_keys:
+            if os.path.exists(json_file):            
+                with open(json_file, 'r', encoding='utf-8') as j:
+                    json_content = json.load(j)
+                    properties = json_content['properties']
 
-                        try:
-                            element = properties[primary_key]
-                        except:
-                            report = 'Primary Key {0} not found in {1}'.format(primary_key, json_file.replace('../../', ''))
-                            issue_list.append(report)
-                            issues += 1
+                    if len(primary_keys) > 0:
+                        for primary_key in primary_keys:
 
-                if len(replication_keys) > 0:
-                    for replication_key in replication_keys:
+                            try:
+                                element = properties[primary_key]
+                            except:
+                                report = 'Primary Key {0} not found in {1}'.format(primary_key, json_file.replace('../../', ''))
+                                issue_list.append(report)
+                                issues += 1
 
-                        try:
-                            element = properties[replication_key]
-                        except:
-                            report = 'Replication Key {0} not found in {1}'.format(replication_key, json_file.replace('../../', ''))
-                            issue_list.append(report)
-                            issues += 1
+                    if len(replication_keys) > 0:
+                        for replication_key in replication_keys:
+
+                            try:
+                                element = properties[replication_key]
+                            except:
+                                report = 'Replication Key {0} not found in {1}'.format(replication_key, json_file.replace('../../', ''))
+                                issue_list.append(report)
+                                issues += 1
+            else:
+                report = '{} file not found'.format(json_file.replace('../../', ''))
+                issue_list.append(report)
+                issues += 1
 
     return [issues, issue_list]
 
@@ -97,38 +102,43 @@ def checkForeignKeys(file, json_folder, issue_list):
 
             json_file = '{0}/{1}.json'.format(json_folder, table_name)
 
-            with open(json_file, 'r', encoding='utf-8') as j:
-                json_content = json.load(j)
-                properties = json_content['properties']
+            if os.path.exists(json_file):
+                with open(json_file, 'r', encoding='utf-8') as j:
+                    json_content = json.load(j)
+                    properties = json_content['properties']
 
-                targets = table['join']
-                for target in targets:
-                    keys = target['keys']
-                    for key in keys:
-                        source_key = key['key']
-                        
-                        steps = source_key.split('.')
-                        steps_count = len(steps)
+                    targets = table['join']
+                    for target in targets:
+                        keys = target['keys']
+                        for key in keys:
+                            source_key = key['key']
+                            
+                            steps = source_key.split('.')
+                            steps_count = len(steps)
 
-                        step = 1
-                        try:
-                            path = properties[steps[0]]
-                            if steps_count == 1:
-                                pass
-                            else:
-                                while step < steps_count :
-                                    try:
-                                        path = path[steps[step]]
-                                    except:
+                            step = 1
+                            try:
+                                path = properties[steps[0]]
+                                if steps_count == 1:
+                                    pass
+                                else:
+                                    while step < steps_count :
                                         try:
-                                            path = path['properties'][steps[step]]
-                                        
+                                            path = path[steps[step]]
                                         except:
-                                            path = path['items']['properties'][steps[step]]
-                                    step +=1
-                        except:
-                            report = 'Key {0} not found in {1}'.format(source_key, json_file.replace('../../', ''))
-                            issue_list.append(report)
-                            issues += 1
+                                            try:
+                                                path = path['properties'][steps[step]]
+                                            
+                                            except:
+                                                path = path['items']['properties'][steps[step]]
+                                        step +=1
+                            except:
+                                report = 'Key {0} not found in {1}'.format(source_key, json_file.replace('../../', ''))
+                                issue_list.append(report)
+                                issues += 1
+            else:
+                report = 'JSON file {} not found'.format(json_file.replace('../../', ''))
+                issue_list.append(report)
+                issues += 1
 
     return [issues, issue_list]
