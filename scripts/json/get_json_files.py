4d60bffc398d5ec83cbfa3dc45547c0a9c3852d5
Lucie Milan <lmilan@talend.com>
2023-10-16 17:53:48 +0200

    TDL-24280 started documenting scripts and workflows

--- scripts/json/get_json_files.py
+++ scripts/json/get_json_files.py
@@ -12,12 +12,14 @@ github_headers = {'Authorization': github_token}
 # Get repository name
 repo = sys.argv[2]
 
+# Get branch if specified
 try:
     branch = sys.argv[3]
 except:
     branch = ''
 
 def getTags(repo):
+    # To be removed
 
     major_versions = []
     version_tags = []
@@ -40,6 +42,7 @@ def getTags(repo):
     return version_tags
 
 def getTapData(setup_file):
+    # Get the name of the tap and the current version from the setup file
 
     name_pattern = re.compile(r'name=(\"|\')([^\'\"]+)(\'|\")')
     version_pattern = re.compile(r'version=(\"|\')([^\'\"]+)(\'|\")')
@@ -47,66 +50,69 @@ def getTapData(setup_file):
     tap_name = re.search(name_pattern, setup_file).group(2)
     tap_version = re.search(version_pattern, setup_file).group(2)
 
+    # Extract the major version
     tap_major_version = tap_version[0]
 
     return [tap_name, tap_major_version]
 
 def getFiles(repo, branch):
+    # Get all files from the selected tap
 
+    # Download zip file with contents of the repo (from specific branch if specified)
     contents_api = host + '/repos/singer-io/' + repo + '/zipball'
-
     if branch != '' and branch != 'all':
         contents_api = contents_api + '/' + branch
-
     repo_contents = requests.get(contents_api, headers=github_headers)
-    status_code = repo_contents.status_code
 
+    # Stop process in case of API issue
+    status_code = repo_contents.status_code
     if status_code != 200:
         sys.exit('API issue\n Status: {}\nMessage:'.format(status_code, repo_contents.content))
     else:
+        # Save, unzip, and delete file
         output_dir = '../../../'
         output_file = '{0}{1}.zip'.format(output_dir, repo)
         zip_output = output_dir + repo
-
         with open(output_file, 'wb') as f:
             f.write(repo_contents.content)
-        
         with zipfile.ZipFile(output_file, 'r') as zip_file:
             zip_file.extractall(zip_output)
-        
         os.remove(output_file)
-        
-        tap_folder = zip_output + '/' + os.listdir(zip_output)[0]
 
+        # Open tap folder, find setup file and extract tap name and version        
+        tap_folder = zip_output + '/' + os.listdir(zip_output)[0]
         setup_file = open(tap_folder + '/setup.py').read()
         tap_data = getTapData(setup_file)
-
         tap_name = tap_data[0]
         tap_version = tap_data[1]
 
+        # Get integration ID and type from '_data/taps/integrations.yml'
         integration_data = getIntegrationData(tap_name)
         integration_id = integration_data[0]
         integration_type = integration_data[1]
 
+        # Get integration version folder
         folder = '../../_data/taps/schemas/{0}/v{1}'.format(integration_id, tap_version)
+        json_output_folder = '{}/json'.format(folder)
 
+        # Ignore integrations with type 'database' since they don't have pre-defined schemas
         if integration_type != 'database':
 
+            # Get list of files to ignore (if any)
             ignore= []
-            
             ignore_file = '{0}/{1}-v{2}-to-ignore.yml'.format(folder, integration_id, tap_version)
             if os.path.exists(ignore_file):
                 with open(ignore_file, 'r', encoding='utf-8') as i:
                     to_ignore = yaml.safe_load(i)
                     ignore = to_ignore['schemas']
-
-            json_output_folder = '{}/json'.format(folder)
-
+            
+            # Create JSON folder if it doesn't exist
             if os.path.exists(json_output_folder):
                 pass
             else:
                 os.makedirs(json_output_folder)
 
+            # Get schema folder and use the function in format_json.py to format the files and get the list of schemas
             for item in os.listdir(tap_folder):
                 item_path = tap_folder + '/' + item
 
@@ -116,22 +122,25 @@ def getFiles(repo, branch):
 
                     schema_list = format_results[0]
                     issue_list = format_results[1]
-
+                    
+                    # Remove files for schemas to ignore
                     for schema in schema_list:
                         if schema in ignore:
                             schema_list.remove(schema)
                             os.remove('{0}/{1}.json'.format(json_output_folder, schema))
                             issue_list.append('JSON file {}.json ignored'.format(schema))
-                    
+
+                    # Use function from 'get_table_data.py' to get and update the yaml file with table details
                     getTableData(integration_id, tap_version, schema_list)
 
+                    # Use function from 'check_table_data.py' to check that the primary keys, replication keys and foreign keys listed are found in the schemas
                     table_issues = checkTableData(integration_id, tap_version)
                     if len(table_issues) > 0:
                         for issue in table_issues:
                             if issue not in issue_list:
                                 issue_list.append(issue)
 
-                    
+                    # Write all issues found to a text file
                     issues_file = '{0}/{1}-v{2}-issues.txt'.format(folder, integration_id, tap_version)
                     issues_count = len(issue_list)
 
@@ -143,7 +152,8 @@ def getFiles(repo, branch):
         
         else:
             print('Ignoring database integration {}'.format(integration_id))
-        
+
+        # Delete tap folder        
         shutil.rmtree(zip_output)
 
 
@@ -166,6 +176,7 @@ def getIntegrationVersions(integration):
     return len(integration_versions)
 
 def getAllTaps(branch):
+    # To remove
     issues = []
 
     file = '../../_data/taps/integrations.yml'
