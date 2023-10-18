1ea1e320ee501259341c9f201e934622359e1014
Lucie Milan <lmilan@talend.com>
2023-10-18 18:34:15 +0200

    Update get_integration_data.py

--- scripts/json/get_integration_data.py
+++ scripts/json/get_integration_data.py
@@ -1,13 +1,16 @@
 import yaml
 
 def getIntegrationData(repo):
-    file = '../../_data/taps/integrations.yml'
+    # From a tap repo name, get the integration ID and type
 
+    file = '../../_data/taps/integrations.yml'
     with open(file, 'r') as f:
         data = yaml.safe_load(f)
         
+        # Get the list of integrations from the file
         integrations = data['integrations']
 
+        # Get and return the ID and type
         for i in integrations:
             tap = integrations[i]['tap']
             id = integrations[i]['id']
