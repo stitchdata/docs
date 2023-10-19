fd6b346cd9aab43b9fa12d8f3f4a31485a101332
Lucie Milan <lmilan@talend.com>
2023-10-19 12:03:44 +0200

    Update get_table_data.py

--- scripts/json/get_table_data.py
+++ scripts/json/get_table_data.py
@@ -1,28 +1,35 @@
 import os, yaml
 
 def compareTableData(existing, new):
-    existing_details = existing['table-details']
-    new_details = new['table-details']
+    # Compare and merge existing table details with table details from the catalog file
 
     updated = existing
+    existing_details = existing['table-details']
+    new_details = new['table-details']
     updated_details = updated['table-details']
 
     if existing_details == new_details:
         pass
     else:
+
+        # If the existing and new details are different check each element
         for element in new_details:
             if element in existing_details:
+
+                # Check if the element in the new details is already in the existing details
                 existing_element = existing_details[element]
                 new_element = new_details[element]
 
-                if type(existing_element) == list:
-                    existing_element = sorted(existing_element)
-
+                # If the value of the element is different, keep the newest value
                 if existing_element == new_element:
                     pass
                 else: 
                     updated_details[element] = new_element
             else:
+
+                # If the element isn't in the existing details: 
+                # - deduplicate replication-key(s) and primary-key(s) elements
+                # - add the element to the table details
                 if element == 'replication-key' and 'replication-keys' in existing_details:
                     updated_details['replication-key'] = new_details['replication-key']
                     updated_details.pop('replication-keys')
@@ -41,31 +48,29 @@ def compareTableData(existing, new):
     return updated
 
 def updateTableData(integration, version, name, data):
+    # Update table details from catalog file
 
-    new = []
-    
+    # Get current table data
     folder = '../../_data/taps/schemas/{0}/v{1}'.format(integration, version)
     file = '{2}/{0}-v{1}-tables.yml'.format(integration, version, folder)
-
     existing_tables = []
 
     if os.path.exists(file):
         with open(file, 'r') as f:
-            existing_data = yaml.safe_load(f)
 
+            # Get list of existing tables
+            existing_data = yaml.safe_load(f)
             tables = existing_data['tables']
 
+            # Look for the current table in the existing tables and compare the existing and new table details
             for table in tables:
                 table_name = table['name']
                 existing_tables.append(table_name)
-
                 if table_name == name:
-                    print(table_name)
                     table = compareTableData(table, data)
             
+            # Add data for new tables
             if name not in existing_tables:
-                new.append(name)
-
                 table = {
                     'name': name,
                     'table-details': {}
@@ -73,20 +78,22 @@ def updateTableData(integration, version, name, data):
                 table = compareTableData(table, data)
                 tables.append(table)
 
+    # Write the updated table data to the YAML file
     with open (file, 'w', encoding='utf-8') as out:
         yaml.dump(existing_data, out, default_flow_style=False, sort_keys=False)    
 
                 
 def getTableData(integration, version, schema_list):
+    # Get table data for JSON schemas
 
     table_list = []
     not_found = []
-
     new = []
     
     folder = '../../_data/taps/schemas/{0}/v{1}'.format(integration, version)
     file = '{2}/{0}-v{1}-tables.yml'.format(integration, version, folder)
 
+    # Get the list of existing tables
     if os.path.exists(file):
         with open(file, 'r') as f:
             data = yaml.safe_load(f)
@@ -96,6 +103,8 @@ def getTableData(integration, version, schema_list):
             for table in tables:
                 table_name = table['name']
                 table_list.append(table_name)
+
+    # If the tables YAML file doesn't exist, create it
     else:
         if os.path.exists(folder):
             pass
@@ -108,11 +117,12 @@ def getTableData(integration, version, schema_list):
             'tables': []
         }
 
+    # For JSON schema found, check if the table is in the list of existing tables
     for table in schema_list:
+
+        # Add the table if it doesn't exist
         if table not in table_list:
-
             new.append(table)
-
             table_data = {
                 'name': table,
                 'description': '',
@@ -127,11 +137,12 @@ def getTableData(integration, version, schema_list):
                     'replication-key': ''
                 }
             }
-
             data['tables'].append(table_data)
 
-    
+    # For each table in the list of existing tables, check if the JSON schema exists
     for table in table_list:
+
+        # If the schema doesn't exist, mark the table as 'not found', it will hide it from the docs but the table data will still exist in the YAML file
         if table not in schema_list:
             status = 'not_found'
             for t in data['tables']:
@@ -139,6 +150,8 @@ def getTableData(integration, version, schema_list):
                     t['status'] = status
                     not_found.append(table)
         else:
+
+            # If the schema exists and the table has the 'not found' status, remove it the status 
             for t in data['tables']:
                 if t['name'] == table: 
                     try:
@@ -146,9 +159,11 @@ def getTableData(integration, version, schema_list):
                     except:
                         pass
 
+    # Write the updated data to the YAML file
     with open (file, 'w', encoding='utf-8') as out:
         yaml.dump(data, out, default_flow_style=False, sort_keys=False)
 
+    # Report new tables and schemas not found
     if len(new) > 0:
         print('The following tables have been added to {}:'.format(file.replace('../../', '')))
         print(*new, sep='\n')
